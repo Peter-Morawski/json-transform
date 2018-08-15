c2b1cb54e6fe2586689dd6071928d81ef25ca15f
Peter Morawski <contact@peter-morawski.de>
2018-08-15 17:42:04 +0200

    * changed the way the utc offset for new york is determined in the tests

--- tests/common.py
+++ tests/common.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import time
+import datetime
+from dateutil import tz
 
 
 def get_new_york_utc_offset():
-    local_time = time.localtime()
-    if local_time.tm_isdst == 1:
-        return "-0400"
-
-    return "-0500"
+    return datetime.datetime.now(tz.gettz("America/New_York")).strftime("%z")
 
 
 def get_new_york_utc_offset_as_int():
-    local_time = time.localtime()
-    if local_time.tm_isdst == 1:
-        return -4
-
-    return -5
+    return int(int(get_new_york_utc_offset()) / 100)
